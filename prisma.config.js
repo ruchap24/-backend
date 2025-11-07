794d5a6d0bacbef511957cafa179f096355a4226
ruchap24 <patilrucha991@gmail.com>
2025-11-07 08:50:34 +0530

    updated

--- prisma.config.js
+++ prisma.config.js
@@ -1,12 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var config_1 = require("prisma/config");
-var dotenv = require("dotenv");
-// Load environment variables from .env file
-dotenv.config();
-// Get DATABASE_URL from environment
-// This will be loaded from .env file or process.env
-var databaseUrl = process.env.DATABASE_URL || "";
+require('dotenv').config();
+
+const databaseUrl = process.env.DATABASE_URL || "";
+
+/**
+ * @type {import('@prisma/client').Prisma.Config}
+ */
+module.exports = {
+  schema: "./prisma/schema.prisma",
+  datasources: {
+    db: {
+      provider: "postgresql",
+      url: databaseUrl,
+    },
+  },
+};
 exports.default = (0, config_1.defineConfig)({
     schema: "prisma/schema.prisma",
     migrations: {
