384bef5568f739c90fdd97b4e7fcd1aa5b939e7d
ruchap24 <patilrucha991@gmail.com>
2025-11-09 23:24:58 +0530

    updated

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,28 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  // Swagger setup
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://i2interfaces.vercel.app',
+      /\.vercel\.app$/, 
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Professional Profile API')
     .setDescription('The Professional Profile API description')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  // CORS setup
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://i2interfaces.vercel.app/',
-      /\.vercel\.app$/,
-    ],
-    credentials: true,
-  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   
